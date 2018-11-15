731885f1684107aecc1ae7909558da92f127d506
JulieLy8619 <july8619@yahoo.com>
2018-11-14 18:57:48 -0800

    got dropdown list and acknowledging a selection from dropdown list

---
+++ js/julieapp.js
@@ -0,0 +1,80 @@
+'use strict';
+
+
+//Feature 1, display images
+//make a constructor funtion
+function Horns (obj) {
+  this.url = obj.image_url;
+  this.title = obj.title;
+  this.description = obj.description;
+  this.keyword = obj.keyword;
+  this.horn = obj.horns;
+}
+Horns.allHornsArray = [];
+Horns.listArrayKeys = [];
+Horns.listArray = [];
+
+Horns.prototype.render = function() {
+  $('main').append('<div class="clone"></div>')
+  let hornClone = $('div[class="clone"]');
+  let hornHtml = $('#photo-template').html();
+  hornClone.html(hornHtml);
+  hornClone.find('h2').text(this.title);
+  hornClone.find('img').attr('src', this.url);
+  hornClone.find('p').text(this.description);
+  hornClone.removeClass('clone');
+  hornClone.attr('class', this.title);
+}
+
+Horns.prototype.makeList = function () {
+  // console.log('Horns.listArray ' + Horns.listArray);
+  // console.log('this.keyword ' + this.keyword);
+  if (!Horns.listArrayKeys.includes(this.keyword)) {
+    // console.log('in makelist');
+    Horns.listArrayKeys.push(this.keyword);
+    Horns.listArray.push(this);
+  }
+}
+
+Horns.prototype.list = function () {
+  let filterList = $('select');
+  filterList.append($('<option></option>').val(this.keyword).html(this.keyword))
+};
+
+//get information from json and populate template, which also then renders to screen.
+Horns.readJson = () => {
+  $.get('data/page-1.json', 'json')
+    .then(data => {
+      data.forEach(obj => {
+        Horns.allHornsArray.push(new Horns(obj));
+      })
+    })
+    .then(Horns.loadHorns)
+    .then(Horns.populateList)
+    .then(Horns.populateForm)
+}
+
+Horns.loadHorns = () => {
+  Horns.allHornsArray.forEach(horn => horn.render());
+}
+
+Horns.populateForm= () => {
+  Horns.listArray.forEach(horn => horn.list());
+}
+
+Horns.populateList = () => {
+  Horns.allHornsArray.forEach(horn => horn.makeList());
+}
+
+$(() => Horns.readJson());
+
+
+//clickhandler
+Horns.filterHandler = () => {
+  // console.log('in handler');
+}
+
+$('select').change(Horns.filterHandler);
+
+
+
