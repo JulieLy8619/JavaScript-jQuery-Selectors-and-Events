fac8ffec6087fae9a1568cb2f7080dbbd900395f
jared <jaredpattison@users.noreply.github.com>
2018-11-18 16:10:19 -0800

    pagination complete

--- js/app.js
+++ js/app.js
@@ -7,11 +7,17 @@ function Horns (obj) {
   this.description = obj.description;
   this.keyword = obj.keyword;
   this.horn = obj.horns;
+  this.page = obj.page;
+
+  Horns.allHornsArray.push(this);
 }
 Horns.allHornsArray = [];
-Horns.listArrayKeys = [];
-Horns.listArray = [];
+Horns.listArrayKeys1 = [];
+Horns.listArray1 = [];
 Horns.filteredListArray = [];
+Horns.listArrayKeys2 = [];
+Horns.listArray2 = [];
+let $whereToGo = 'page1';
 
 Horns.prototype.render = function() {
   const source = $('#photo-template').html();
@@ -19,48 +25,96 @@ Horns.prototype.render = function() {
   return template(this);
 };
 
-Horns.prototype.makeList = function () {
-  if (!Horns.listArrayKeys.includes(this.keyword)) {
-    Horns.listArrayKeys.push(this.keyword);
-    Horns.listArray.push(this);
+Horns.prototype.makeList1 = function () {
+  if (!Horns.listArrayKeys1.includes(this.keyword)) {
+    Horns.listArrayKeys1.push(this.keyword);
+    Horns.listArray1.push(this);
   }
 }
 
-Horns.prototype.list = function () {
-  let filterList = $('select');
-  filterList.append($('<option></option>').val(this.keyword).html(this.keyword))
+Horns.prototype.makeList2 = function () {
+  if (!Horns.listArrayKeys2.includes(this.keyword)) {
+    Horns.listArrayKeys2.push(this.keyword);
+    Horns.listArray2.push(this);
+  }
+}
+
+Horns.prototype.list1 = function () {
+  let filterList1 = $('select[name="page1"');
+  filterList1.append($('<option></option>').val(this.keyword).html(this.keyword))
+};
+
+Horns.prototype.list2 = function () {
+  let filterList2 = $('select[name="page2"');
+  filterList2.append($('<option></option>').val(this.keyword).html(this.keyword))
 };
 
 Horns.prototype.clearImage = function (){
   $('div').remove();
 }
 
+$('ul a').on('click', function() {
+  $whereToGo = $(this).data('tab');
+  $('.photo-location').hide();
+  Horns.loadHorns();
+  $('#' + $whereToGo).fadeIn(750)
+  if($whereToGo === 'page1') {
+    $('select[name="page2"').hide();
+    $('select[name="page1"').fadeIn(750);
+  }else{
+    $('select[name="page1"').hide();
+    $('select[name="page2"').fadeIn(750);
+  }
+})
+
 //get information from json and populate template, which also then renders to screen.
 Horns.readJson = () => {
   $.get('data/page-1.json', 'json')
     .then(data => {
       data.forEach(obj => {
-        Horns.allHornsArray.push(new Horns(obj));
+        obj.page = 'page1';
+        new Horns(obj);
+      })
+    })
+  $.get('data/page-2.json', 'json')
+    .then(data => {
+      data.forEach(obj => {
+        obj.page = 'page2';
+        new Horns(obj);
       })
     })
     .then(Horns.loadHorns)
-    .then(Horns.populateList)
-    .then(Horns.populateForm)
+    .then($('#page2').hide())
+    .then(Horns.populateLists)
+    .then(Horns.populateForms)
 }
 
 Horns.loadHorns = () => {
   Horns.allHornsArray.forEach(horn => {
-    $('#photo-location').append(horn.render());
+    if(horn.page === 'page1') {
+      $('#page1').append(horn.render());
+    }else{
+      $('#page2').append(horn.render());
+    }
   })
-};
-
-Horns.populateForm= () => {
-  Horns.listArray.forEach( horn => horn.list());
 }
 
-Horns.populateList = () => {
-  Horns.allHornsArray.forEach(horn => horn.makeList());
+Horns.populateLists = () => {
+  Horns.allHornsArray.forEach(horn => {
+    if (horn.page === 'page1') {
+      horn.makeList1();
+    }else{
+      horn.makeList2();
+    }
+  })
 }
+
+Horns.populateForms = () => {
+  Horns.listArray1.forEach( horn => horn.list1());
+  Horns.listArray2.forEach( horn => horn.list2());
+  $('select[name="page2"').hide();
+}
+
 Horns.clearPage = () => {
   Horns.allHornsArray.forEach(horn => horn.clearImage());
 }
@@ -73,7 +127,7 @@ $(() => Horns.readJson());
 //clickhandler
 
 
-$('select').on('change', function(event) {
+$('select[name="page1"').on('change', function(event) {
   //clear images
   Horns.clearPage();
   Horns.filteredListArray = [];
@@ -85,6 +139,22 @@ $('select').on('change', function(event) {
     }
   })
   Horns.filteredListArray.forEach(horn => {
-    $('#photo-location').append(horn.render());
+    $('#page1').append(horn.render());
+  })
+})
+
+$('select[name="page2"').on('change', function(event) {
+  //clear images
+  Horns.clearPage();
+  Horns.filteredListArray = [];
+  let getKey = event.target.value;
+  Horns.allHornsArray.forEach( hornObj => {
+    if (getKey === hornObj.keyword) {
+      Horns.filteredListArray.push(hornObj);
+      hornObj.render();
+    }
+  })
+  Horns.filteredListArray.forEach(horn => {
+    $('#page2').append(horn.render());
   })
 })
